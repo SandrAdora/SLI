93bc41a48de59dc7e1d882decb6b4bec019e01c1
GoodMorningMidnightMirados <sandraedigin@yahoo.com>
2026-01-03 17:44:00 +0100

    used the new mediapipe version to generate the dataset

--- code/backend/ai_agents/prepare_data/create_dataset.py
+++ code/backend/ai_agents/prepare_data/create_dataset.py
@@ -1,51 +1,70 @@
 ###################################################
 # This program creates the dataset for Classifier #
-#                                                 #
 ###################################################
+
 import os
-import mediapipe as mp 
 import cv2
-import matplotlib.pyplot as plt
 import pickle
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-# setting up handmarks
-mp_hands = mp.solutions.hands
-mp_drawings = mp.solutions.drawing_utils
-mp_drawing_styles=mp.solutions.drawing_styles
+# Path to your augmented images
+DATA = './data/augmented/'
 
+# Path to the new MediaPipe model
+MODEL_PATH = 'models/hand_landmarker.task'
 
-hands=mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3) 
+# Load the new MediaPipe Hand Landmarker
+base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
+options = vision.HandLandmarkerOptions(
+    base_options=base_options,
+    num_hands=1,
+    min_hand_detection_confidence=0.3,
+    min_hand_presence_confidence=0.3,
+    min_tracking_confidence=0.3
+)
+landmarker = vision.HandLandmarker.create_from_options(options)
 
-# Get the Images
-DATA ='./data'
+# Storage for dataset
+data = []
+labels = []
 
-# Store the datasets
-data=[]
-labels=[]
-
-# detect the landmarks and extract the x and y coordinates of the landmarks
+# Process each class folder
 for dir_ in os.listdir(DATA):
-    for img_pth in os.listdir(os.path.join(DATA, dir_)):
-        data_xy=[]
-        img = cv2.imread(os.path.join(DATA, dir_, img_pth))
-        img_rgb=cv2.cvtColor(img , cv2.COLOR_BAYER_BG2BGR)
-        
-        # detect all the landmarks into the image
-        result=hands.process(img_rgb)
-        if result.multi_hand_landmarks:
-            for hand_landmarks in result.multi_hand_landmarks:
-                # Get the x and y coordinates from the img which will be our
-                # X-dataset and Y dataset 
-                for i in range(len(hand_landmarks.landmark)):
-                    X =  hand_landmarks.landmark[i].x 
-                    y = hand_landmarks.landmark[i].y
-                    data_xy.append(X)
-                    data_xy.append(y)
-            data.append(data_xy)
-            labels.append(dir_)
-        
+    class_path = os.path.join(DATA, dir_)
 
-# Generate the file data.pickle and store dataset from lists data and labels in it      
-f=open('data.pickle', 'wb')
-pickle.dump({'data': data, 'labels':labels}, f)
-f.close()
+    for img_pth in os.listdir(class_path):
+        img_path = os.path.join(class_path, img_pth)
+
+        # Load image
+        img = cv2.imread(img_path)
+        if img is None:
+            print(f"Could not load image: {img_path}")
+            continue
+
+        # Convert to MediaPipe Image
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+
+        # Run hand landmark detection
+        result = landmarker.detect(mp_image)
+
+        if not result.hand_landmarks:
+            continue
+
+        # Extract x,y coordinates
+        data_xy = []
+        hand = result.hand_landmarks[0]
+
+        for lm in hand:
+            data_xy.append(lm.x)
+            data_xy.append(lm.y)
+
+        data.append(data_xy)
+        labels.append(dir_)
+
+# Save dataset
+with open('data.pickle', 'wb') as f:
+    pickle.dump({'data': data, 'labels': labels}, f)
+
+print("Dataset created successfully!")
